ac8d373d35dd40701c83ce3bb5e9038d1e309ec3
Nino Nikoles <nnikoles@gmail.com>
2023-06-23 17:27:09 +0200

    Update watch.php
    Removed test span

--- views/templates/watch.php
+++ views/templates/watch.php
@@ -4,6 +4,5 @@ include(ROOT_PATH.'/views/head.php');
 echo '<div id="mainPlayer" class="fullscreen">';
     videoPlayer($_GET['id'], true);
 echo '</div>';
-echo '<span id="test"></span>';
 
 include('views/footer.php'); ?>
