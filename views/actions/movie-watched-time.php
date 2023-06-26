79dd6ddc5c9d37568643afc45f6ee4cf5d77823a
Nino Nikoles <nnikoles@gmail.com>
2023-06-26 11:03:23 +0200

    Update movie-watched-time.php
    Strings to numbers

--- views/actions/movie-watched-time.php
+++ views/actions/movie-watched-time.php
@@ -11,11 +11,11 @@
 
     if ( $watchedInPercent === 100 ) {
         $sql = "INSERT INTO movie_watched(user_id, movie_id, watched_seconds, total_length, watched) VALUES
-        ('$userID', '$movieID', '$watchedTime', '$totalLength', 1)
+        ($userID, $movieID, $watchedTime, $totalLength, 1)
         ON DUPLICATE KEY UPDATE watched_seconds = VALUES(watched_seconds)";
     } else {
         $sql = "INSERT INTO movie_watched(user_id, movie_id, watched_seconds, total_length) VALUES
-        ('$userID', '$movieID', '$watchedTime', '$totalLength')
+        ($userID, $movieID, $watchedTime, $totalLength)
         ON DUPLICATE KEY UPDATE watched_seconds = VALUES(watched_seconds)";
     }
 
