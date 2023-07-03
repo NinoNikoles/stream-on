f4c45169c72e2fb9d2de02d51b127e41c800a76a
Nino Nikoles <nnikoles@gmail.com>
2023-07-03 16:14:53 +0200

    Infinite scroll for movie page now working (hopefully)

--- views/actions/movie-scroll-load.php
+++ views/actions/movie-scroll-load.php
@@ -2,75 +2,34 @@
 $conn = dbConnect();
 $tmdb = setupTMDB();
 
-$loadCount = $_POST['count'];
+if ( $_POST['count'] === '' ) {   
+    $loadCount = 0;
+} else {
+    $loadCount = $_POST['count'];
+}
+
 $movies = scrollLoader('movies', $loadCount);
+
+$movieList = '';
 
 if ( $movies > 0 ) {    
     foreach ( $movies as $movie ) {
-        $movieID = $movie['id'];
-        $movieTitle = $movie['title'];
-        $movieOverview = $movie['overview'];
-        $movieRating = $movie['voteAverage'];
-        $movieRuntime = $movie['runtime'];
-        $movieRelease = new DateTime($movie['release']);
-        $releaseYear = $movieRelease->format('Y');
-        $moviePoster = $movie['poster'];
-        $movieBackdrop = $movie['backdrop'];
-        $genres = $movie['genres'];
-        $genreHTML = '';
-        foreach ( $genres as $genre ) {
-            $genreHTML = $genreHTML . '<span class="tag">'.$genre['name'].'</span>';
-        }
-
-        echo '<div class="col-6 col-3-medium grid-padding">';
-            echo '<div class="media-card widescreen-media-card desktop-only">
-                    <figure class="widescreen">
-                        <img src="'.$tmdb->getImageURL().$movieBackdrop.'" alt="">
-                    </figure>
-                    <div class="link-wrapper">
-                        <a href="/watch/?id='.$movieID.'" title="'.$movieTitle.'" class="play-trigger"></a>
-                        <a href="#content-'.$movieID.'" title="'.lang_snippet('more_informations').'" class="info-trigger" data-modal data-src="#content-'.$movieID.'"></a>
-                    </div>
-                </div>
-
-                <div class="media-card mobile-only">
-                    <figure class="poster">
-                        <img src="'.$tmdb->getImageURL().$moviePoster.'" alt="">
-                    </figure>
-                    <div class="link-wrapper">
-                        <a href="/watch/?id='.$movieID.'" title="'.$movieTitle.'" class="play-trigger"></a>
-                        <a href="#content-'.$movieID.'" title="'.$movieTitle.'" class="info-trigger" data-modal data-src="#content-'.$movieID.'"></a>
-                    </div>
-                </div>';
-
-            echo '<div class="info-popup" id="content-'.$movieID.'" style="display:none;">';
-                echo '<div class="col12 marg-bottom-xs mobile-only">';
-                    echo '<figure class="widescreen">';
-                        echo '<img src="'.$tmdb->getImageURL().$movieBackdrop.'">';
-                    echo '</figure>';
-                echo '</div>';
-                echo '<div class="innerWrap">';
-                    echo '<div class="col7 marg-right-col1">';
-                        echo '<p class="h2">'.$movieTitle.'</p>';
-                        echo '<p class="small">';
-                            echo '<span class="tag">'.$releaseYear.'</span>';
-                            echo '<span class="tag">'.$movieRating.'/10</span>';
-                            echo '<span class="tag">'.runtimeToString($movieRuntime).'</span>';
-                        echo '</p>';
-                        echo '<a href="/watch/?id='.$movieID.'" class="btn btn-white icon-left icon-play">Jetzt schauen</a>';
-                        echo '<p class="small">'.$movieOverview.'</p>';
-                        echo '<p class="small">'.$genreHTML.'</p>';
-                    echo '</div>';
-                    echo '<div class="col4 desktop-only">';
-                        echo '<figure class="poster">';
-                            echo '<img src="'.$tmdb->getImageURL().$moviePoster.'" alt="">';
-                        echo '</figure>';
-                    echo '</div>';
-                echo '</div>';
-            echo '</div>';
-        echo '</div>';
+        $movieList = $movieList . movie_card($movie, 'col-6 col-3-medium grid-padding');
     }
 
     $loadCount = count($movies);
-    echo '<span class="load-count" style="display: none;">'.$loadCount.'</span>';
+
+    $response = array(
+        'movieList' => $movieList,
+        'loadCount' => $loadCount,
+    );
+
+    echo json_encode($response);
+} else {
+    $response = array(
+        'movieList' => 0,
+        'loadCount' => 0,
+    );
+
+    echo json_encode($response);
 }
