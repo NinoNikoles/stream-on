ac94d8efb6c70f205c0c4945fbbf96cc02875d79
Nino Nikoles <nnikoles@gmail.com>
2023-07-04 15:21:39 +0200

    Genre sliders as function

--- src/admin/modules/container.php
+++ src/admin/modules/container.php
@@ -82,6 +82,7 @@ function movie_card($movie, $extraClasses = '') {
                 </div>
             </div>
         </div>';
+    $conn->close();
     return $card;
 }
 
@@ -210,6 +211,8 @@ function currentWatchlist() {
             echo '</div>';
         echo '</div>';
     }
+
+    $conn->close();
 }
 
 function myList() {
@@ -225,5 +228,77 @@ function myList() {
             echo movie_card($movie, 'col-6 col-3-medium grid-padding');
         }
     }
+
+    $conn->close();
+}
+
+function genreSlider() {
+    $conn = dbConnect();
+    $tmdb = setupTMDB();
+
+    $sql = "SELECT genre_id, genre_name FROM genres ORDER BY genre_id ASC";
+    $results = $conn->query($sql);
+
+    if ($results->num_rows > 0) {
+        $sliderNumber = 1;
+
+        while ($genre = $results->fetch_assoc()) {
+            $movieRow = goTrhoughMovies($genre['genre_id'], $conn, $tmdb);
+            
+            if ( $movieRow != '' ) {
+                $genre_slider = 'genre-slider-'.$sliderNumber;
+
+                echo '<div class="genre-slider '.$genre_slider.'">';
+                    echo '<div class="col12 marg-top-l">';
+                        echo '<div class="column">';
+                            echo '<h3>'.$genre['genre_name'].'</h3>';
+                        echo '</div>';
+
+                        echo '<div class="column">'; 
+                            echo '<div class="swiper card-slider">';
+                                echo '<div class="swiper-wrapper">';
+                                    echo $movieRow;
+                                echo '</div>';
+                                echo '<div class="swiper-button-prev"></div>
+                                <div class="swiper-button-next"></div>';
+                            echo '</div>';
+                        echo '</div>';
+                    echo '</div>';
+                echo '</div>';
+                $sliderNumber++;
+            }
+        }
+    } else {
+        echo '<div class="innerWrap marg-top-l">';
+            echo '<div class="col12">';
+                echo '<p>Keine Genres vorhanden!</p>';
+            echo '</div>';
+        echo '</div>';
+    }
+
+    function goTrhoughMovies($db_genre, $conn, $tmdb) {
+        $genreID = intval($db_genre);
+        
+        $query = "SELECT movie_tmdbID, movie_title, movie_tagline, movie_overview, movie_poster, movie_thumbnail, movie_rating, movie_release, movie_runtime, movie_genres FROM movies INNER JOIN movie_genre ON movies.movie_tmdbID = movie_genre.movie_id WHERE movie_genre.genre_id = $genreID ORDER BY RAND() LIMIT 20";
+        $result = $conn->query($query);
+        
+        $movieRow = '';
+
+        if ($result->num_rows > 0) {
+            // Es gibt mindestens einen Film des Genres
+        
+            while ($movie = $result->fetch_assoc()) {
+                $movieRow = $movieRow . movie_card($movie, 'swiper-slide');  
+            }
+        } else {
+            $movieRow = '';
+        }
+
+        return $movieRow;
+    }
+
+    echo '<div class="marg-bottom-l"></div>';
+
+    $conn->close();
 }
 ?>
--- views/index.php
+++ views/index.php
@@ -6,68 +6,7 @@ $tmdb = setupTMDB();
 
 currentWatchlist();
 
-
-$sql = "SELECT * FROM genres ORDER BY genre_id ASC";
-$results = $conn->query($sql);
-
-if ($results->num_rows > 0) {
-    $sliderNumber = 1;
-
-    while ($genre = $results->fetch_assoc()) {
-        $movieRow = goTrhoughMovies($genre['genre_id'], $conn, $tmdb);
-        
-        if ( $movieRow != '' ) {
-            $genre_slider = 'genre-slider-'.$sliderNumber;
-
-            echo '<div class="genre-slider '.$genre_slider.'">';
-                echo '<div class="col12 marg-top-l">';
-                    echo '<div class="column">';
-                        echo '<h3>'.$genre['genre_name'].'</h3>';
-                    echo '</div>';
-
-                    echo '<div class="column">'; 
-                        echo '<div class="swiper card-slider">';
-                            echo '<div class="swiper-wrapper">';
-                                echo $movieRow;
-                            echo '</div>';
-                            echo '<div class="swiper-button-prev"></div>
-                            <div class="swiper-button-next"></div>';
-                        echo '</div>';
-                    echo '</div>';
-                echo '</div>';
-            echo '</div>';
-            $sliderNumber++;
-        }
-    }
-} else {
-    echo '<div class="innerWrap marg-top-l">';
-        echo '<div class="col12">';
-            echo '<p>Keine Genres vorhanden!</p>';
-        echo '</div>';
-    echo '</div>';
-}
-
-function goTrhoughMovies($db_genre, $conn, $tmdb) {
-    $genreID = intval($db_genre);
-    
-    $query = "SELECT * FROM movies INNER JOIN movie_genre ON movies.movie_tmdbID = movie_genre.movie_id WHERE movie_genre.genre_id = $genreID ORDER BY RAND() LIMIT 20";
-    $result = $conn->query($query);
-    
-    $movieRow = '';
-
-    if ($result->num_rows > 0) {
-        // Es gibt mindestens einen Film des Genres
-    
-        while ($movie = $result->fetch_assoc()) {
-            $movieRow = $movieRow . movie_card($movie, 'swiper-slide');  
-        }
-    } else {
-        $movieRow = '';
-    }
-
-    return $movieRow;
-}
-echo '<div class="marg-bottom-l"></div>';
+genreSlider();
 
 include(ROOT_PATH.'/views/footer.php');
 ?>
