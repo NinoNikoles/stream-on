96cc8ab30880044dd2029117d54f81e0ef454d36
Nino Nikoles <nnikoles@gmail.com>
2023-07-07 09:46:18 +0200

    Player Styling

--- views/templates/watch.php
+++ views/templates/watch.php
@@ -1,6 +1,7 @@
 <?php 
 include(ROOT_PATH.'/views/head.php');
 
+echo '<style>body { padding-top:0!important; }</style>';
 echo '<div id="mainPlayer" class="fullscreen">';
     videoPlayer($_GET['id'], true);
 echo '</div>';
