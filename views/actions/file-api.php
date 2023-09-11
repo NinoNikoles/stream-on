d597db2b612c431b34d7c04686074857f1d51d3a
Nino Nikoles <nnikoles@gmail.com>
2023-09-11 20:26:43 +0200

    Update file-api.php

--- views/actions/file-api.php
+++ views/actions/file-api.php
@@ -1,53 +1,53 @@
 <?php
 
 function getFolderStructure($folderPath) {
-  $folderStructure = array();
-  $files = scandir($folderPath);
+    $folderStructure = array();
+    $files = scandir($folderPath);
 
-  foreach ($files as $file) {
-    if ($file === '.' || $file === '..') {
-      continue;
+    foreach ($files as $file) {
+        if ($file === '.' || $file === '..') {
+            continue;
+        }
+
+        $filePath = $folderPath . '/' . $file;
+
+        if (is_dir($filePath)) {
+            $folderStructure[] = array(
+                'text' => $file,
+                'children' => getFolderStructure($filePath),
+                'icon' => 'jstree-folder',
+            );
+        } else {
+            // Icon basierend auf Dateityp festlegen
+            $extension = pathinfo($file, PATHINFO_EXTENSION);
+            $icon = getIconForExtension($extension);
+
+            $folderStructure[] = array(
+                'text' => $file,
+                'icon' => $icon
+            );
+        }
     }
 
-    $filePath = $folderPath . '/' . $file;
-
-    if (is_dir($filePath)) {
-      $folderStructure[] = array(
-        'text' => $file,
-        'children' => getFolderStructure($filePath),
-        'icon' => 'jstree-folder',
-      );
-    } else {
-      // Icon basierend auf Dateityp festlegen
-      $extension = pathinfo($file, PATHINFO_EXTENSION);
-      $icon = getIconForExtension($extension);
-
-      $folderStructure[] = array(
-        'text' => $file,
-        'icon' => $icon
-      );
-    }
-  }
-
-  return $folderStructure;
+    return $folderStructure;
 }
 
 function getIconForExtension($extension) {
-  // Mapping von Dateierweiterung zu Icon-Klassen
-  $iconMapping = array(
-    'mp4' => 'icon icon-media',
-    // weitere Dateierweiterungen und Icons hier hinzufügen
-  );
+    // Mapping von Dateierweiterung zu Icon-Klassen
+    $iconMapping = array(
+        'mp4' => 'icon icon-media',
+        // weitere Dateierweiterungen und Icons hier hinzufügen
+    );
 
-  // Standard-Icon, wenn keine Übereinstimmung gefunden wurde
-  $defaultIcon = 'jstree-file';
+    // Standard-Icon, wenn keine Übereinstimmung gefunden wurde
+    $defaultIcon = 'jstree-file';
 
-  // Überprüfen, ob ein spezifisches Icon für die Erweiterung vorhanden ist
-  if (isset($iconMapping[$extension])) {
-    return $iconMapping[$extension];
-  }
+    // Überprüfen, ob ein spezifisches Icon für die Erweiterung vorhanden ist
+    if (isset($iconMapping[$extension])) {
+        return $iconMapping[$extension];
+    }
 
-  return $defaultIcon;
+    return $defaultIcon;
 }
 
 $rootFolder = $_SERVER['DOCUMENT_ROOT'].'/media/';
